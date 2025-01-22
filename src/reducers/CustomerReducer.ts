4f76946cbd13c83936f2805ea6c4c7bb3568d804
ramindu-ijse <ramindu.samarawickrama@gmail.com>
2025-01-22 13:35:33 +0530

    init commit

--- src/reducers/CustomerReducer.ts
+++ src/reducers/CustomerReducer.ts
@@ -20,30 +20,6 @@ export const saveCustomer = createAsyncThunk(
     }
 );
 
-export const deleteCustomer = createAsyncThunk(
-    'customer/deleteCustomer',
-    async (email: string) =>{
-        try{
-            const response = await api.delete(`/delete/${id}`);
-            return response.data;
-        }catch(err){
-            console.log(err);
-        }
-    }
-)
-
-export const updateCustomer = createAsyncThunk(
-    'customer/updateCustomer',
-    async (payload:{email: string, customer: Customer}) =>{
-        try{
-            const response = await api.put(`/update/${payload.id}`, payload.customer)
-            return response.data;
-        }catch(err){
-            console.log(err);
-        }
-    }
-);
-
 const customerSlice = createSlice({
     name : 'customer',
     initialState,
@@ -62,33 +38,6 @@ const customerSlice = createSlice({
             })
             .addCase(saveCustomer.pending, (state, action) => {
                 console.error("Pending");
-            });
-        builder
-            .addCase(deleteCustomer.fulfilled, (state, action) => {
-                return state.filter(customer => customer.email !== action.payload);
-            })
-            .addCase(deleteCustomer.rejected, (state, action) => {
-                console.error("Failed to delete customer:", action.payload);
-            })
-            .addCase(deleteCustomer.pending, (state) => {
-                console.log("Deleting customer...");
-            });
-
-        builder
-            .addCase(updateCustomer.fulfilled, (state, action) => {
-                state.map((customer)=>{
-                    if(customer.id === action.payload.id){
-                        customer.name = action.payload.customer.name;
-                        customer.email = action.payload.customer.email;
-                        customer.phone = action.payload.customer.phone;
-                    }
-                })
-            })
-            .addCase(updateCustomer.rejected, (state, action) => {
-                console.error("Failed to save customer:", action.payload); // Handle error
-            })
-            .addCase(updateCustomer.pending, (state, action) => {
-                console.error("Pending"); // Handle error
             });
 
     }
